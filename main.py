f87a2bc6e806769573c181a3b714ef3574f132c7
zachkoelling-eng <zach.koelling@gmail.com>
2026-04-07 19:32:33 -0600

    fixed framerate to 60fps

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -18,6 +20,7 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+        dt = ( clock.tick(60) ) / 1000
 
 
 if __name__ == "__main__":
